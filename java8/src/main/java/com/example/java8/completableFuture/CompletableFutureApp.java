6d43a89294814ab1fd82d03bcd364cefce03dda8
윤지현 <yunnjhh@gmail.com>
2022-06-04 15:48:16 +0900

    feat: use completable future API2

--- java8/src/main/java/com/example/java8/completableFuture/CompletableFutureApp.java
+++ java8/src/main/java/com/example/java8/completableFuture/CompletableFutureApp.java
@@ -1,9 +1,12 @@
 package com.example.java8.completableFuture;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class CompletableFutureApp {
 
@@ -15,6 +18,109 @@ public class CompletableFutureApp {
 
         assignExecutors();
 
+        joinFutures();
+
+        joinFutures2();
+
+        useExceptionHandling();
+
+    }
+
+    private static void useExceptionHandling() throws InterruptedException, ExecutionException {
+        boolean throwError = true;
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            if (throwError) {
+                throw new IllegalArgumentException();
+            }
+            System.out.println("hello: " + Thread.currentThread().getName());
+            return "hello";
+
+        }).exceptionally(ex -> {
+            System.out.println(ex);
+            return "Error!";
+        });
+
+        System.out.println(future.get());
+        System.out.println();
+
+        CompletableFuture<String> handleFuture = CompletableFuture.supplyAsync(() -> {
+            if (throwError) {
+                throw new IllegalArgumentException();
+            }
+            System.out.println("hello: " + Thread.currentThread().getName());
+            return "Hello";
+
+        }).handle((s, e) -> {
+            if (e != null) {
+                System.out.println(e);
+                return "Error!";
+            }
+            return s + " Success!";
+        });
+
+        System.out.println(handleFuture.get());
+    }
+
+    private static void joinFutures2() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> w = CompletableFuture.supplyAsync(() -> {
+            System.out.println("w: " + Thread.currentThread().getName());
+            return "w";
+        });
+
+        CompletableFuture<String> o = CompletableFuture.supplyAsync(() -> {
+            System.out.println("o: " + Thread.currentThread().getName());
+            return "o";
+        });
+
+        CompletableFuture<String> r = CompletableFuture.supplyAsync(() -> {
+            System.out.println("r: " + Thread.currentThread().getName());
+            return "r";
+        });
+
+        CompletableFuture<String> k = CompletableFuture.supplyAsync(() -> {
+            System.out.println("k: " + Thread.currentThread().getName());
+            return "k";
+        });
+
+        CompletableFuture<Void> futures = CompletableFuture.allOf(w, o, r, k)
+                .thenAccept(System.out::println);
+
+        futures.get();
+        System.out.println();
+
+        List<CompletableFuture<String>> futureList = Arrays.asList(w, o, r, k);
+        CompletableFuture[] futureArray = futureList.toArray(new CompletableFuture[futureList.size()]);
+
+        CompletableFuture<List<String>> results = CompletableFuture.allOf(futureArray)
+                .thenApply(v -> futureList.stream().map(CompletableFuture::join).collect(Collectors.toList()));
+        results.get().forEach(System.out::println);
+        System.out.println();
+
+        CompletableFuture<Void> futureAnyOf = CompletableFuture.anyOf(w, o, r, k).thenAccept(System.out::println);
+        futureAnyOf.get();
+    }
+
+    private static void joinFutures() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello: " + Thread.currentThread().getName());
+            return "Hello";
+        });
+
+        CompletableFuture<String> world = getWorld("Hello");
+
+        System.out.println(hello.get());
+        System.out.println(world.get());
+        System.out.println();
+
+        // 두 개의 future 간의 의존성이 있는 경우
+        CompletableFuture<String> composeFuture = hello.thenCompose(CompletableFutureApp::getWorld);
+        System.out.println(composeFuture.get());
+        System.out.println();
+
+        // 두 개의 future 간의 의존성이 없는 경우
+        CompletableFuture<String> combineFuture = hello.thenCombine(world, (h, w) -> h + " " + w);
+        System.out.println(combineFuture.get());
+        System.out.println();
     }
 
     private static void assignExecutors() throws InterruptedException, ExecutionException {
@@ -90,5 +196,12 @@ public class CompletableFutureApp {
             return "우와~";
         });
         System.out.println(futureHasReturn.get());
+    }
+
+    private static CompletableFuture<String> getWorld(String message) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println("World: " + Thread.currentThread().getName());
+            return message + " World";
+        });
     }
 }
